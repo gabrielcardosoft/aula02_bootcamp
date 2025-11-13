795c8b0d11c4ec703ca69d912dffdc10c718772c
Gabriel Cardoso <gcardosof@proton.me>
2025-11-13 11:24:34 -0300

    O exercicio 10 foi resolvido

--- exercicios.py
+++ exercicios.py
@@ -1,10 +1,13 @@
-# Exercício 4
-#
-# Faça um programa que peça dois números interios e imprima a divisão inteira do primeiro pelo segundo.
+# 04. Faça um programa que peça dois números interios e imprima a divisão inteira do primeiro pelo segundo.
 
 first_value = int(input("Digite o primeiro número: "))
 second_value = int(input("Digite o segundo número: "))
-
 divisao = (first_value // second_value)
-
 print(f"A divisão do primeiro valor pelo segundo valor é {divisao}")
+
+# 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada
+
+import math
+raio_circulo = float(input("Digite o raio do círculo: "))
+area_circulo = math.pi * raio_circulo ** 2
+print(f"{area_circulo:.2f}")
