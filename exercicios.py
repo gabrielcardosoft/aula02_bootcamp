6202ec7a1ea247ef05618ff8fe28b2dc98e2e749
Gabriel Cardoso <gcardosof@proton.me>
2025-11-13 11:16:19 -0300

    O exercicio 04 foi resolvido

---
+++ exercicios.py
@@ -0,0 +1,10 @@
+# Exercício 4
+#
+# Faça um programa que peça dois números interios e imprima a divisão inteira do primeiro pelo segundo.
+
+first_value = int(input("Digite o primeiro número: "))
+second_value = int(input("Digite o segundo número: "))
+
+divisao = (first_value // second_value)
+
+print(f"A divisão do primeiro valor pelo segundo valor é {divisao}")
